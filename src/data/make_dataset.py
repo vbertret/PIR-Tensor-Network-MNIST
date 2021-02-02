df38e99989030bcafe81ef24eb3dbc47157bbb9a
victorius35 <victor.bertret@gmail.com>
2021-02-02 10:26:38 +0100

    data

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,6 +1,8 @@
 import numpy as np
 import math
 import gzip
+import matplotlib.pyplot as plt
+
 def make_dataset_easy():
 
     Nt=8 #Nombre de training example
@@ -35,7 +37,9 @@ def make_dataset_random(N,nbExample):
 
     return (data,y)
 
-def make_MNIST_dataset():
+def load_MNIST_dataset():
+
+    #Training images
     f = gzip.open('data/external/train-images-idx3-ubyte.gz','r')
 
     image_size = 28
@@ -43,11 +47,95 @@ def make_MNIST_dataset():
 
     f.read(16)
     buf = f.read(image_size * image_size * num_images)
-    data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
-    data = data.reshape(num_images, image_size, image_size)
+    train_data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
+    train_data = train_data.reshape(num_images, image_size, image_size)
 
-    return data
+    f.close()
+
+    #Test images
+    f = gzip.open('data/external/t10k-images-idx3-ubyte.gz','r')
+
+    image_size = 28
+    num_images = 10000
+
+    f.read(16)
+    buf = f.read(image_size * image_size * num_images)
+    test_data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
+    test_data = test_data.reshape(num_images, image_size, image_size)
+
+    f.close()
+
+    #Training labels
+
+    f = gzip.open('data/external/train-labels-idx1-ubyte.gz','r')
+
+    num_images = 60000
+
+    f.read(8)
+    buf = f.read(1 * num_images)
+    train_labels = np.frombuffer(buf, dtype=np.uint8).astype(np.int64)
+
+    f.close()
+
+    #Test labels
+
+    f = gzip.open('data/external/t10k-labels-idx1-ubyte.gz','r')
+
+    num_images = 10000
+
+    f.read(8)
+    buf = f.read(1 * num_images)
+    test_labels = np.frombuffer(buf, dtype=np.uint8).astype(np.int64)
+
+    f.close()
+
+    return (train_data , test_data , train_labels , test_labels)
+
+def convert_one_hot_encoding(labels):
+    num_labels=labels.shape[0]
+    num_class = np.max(labels) + 1
+    new_labels=np.zeros((num_labels,num_class))
+
+    for i in range(num_labels):
+        new_labels[i,labels[i]]=1
+
+    return new_labels
+
+def make_MNIST_dataset_small():
+
+    train_data , test_data , train_labels , test_labels = load_MNIST_dataset()
+
+    #Training images
+    num_images = 60000
+
+    new_train_data=np.zeros((num_images,14,14))
+    for k in range(num_images):
+        for i in range(0,28,2):
+            for j in range(0,28,2):
+                new_train_data[k,int(i/2),int(j/2) ] = (train_data[k,i,j] + train_data[k,i+1,j] + train_data[k,i,j+1] + train_data[k,i+1,j+1])/4
+
+    #Training images
+    num_images = 10000
+
+    new_test_data=np.zeros((num_images,14,14))
+    for k in range(num_images):
+        for i in range(0,28,2):
+            for j in range(0,28,2):
+                new_test_data[k,int(i/2),int(j/2) ] = (test_data[k,i,j] + test_data[k,i+1,j] + test_data[k,i,j+1] + test_data[k,i+1,j+1])/4
+
+    
+    np.save("data/processed/small_train_images.npy",new_train_data)
+
+    return (new_train_data , new_test_data , train_labels , test_labels)
+
 
 if __name__ == "__main__":
-    data=make_MNIST_dataset()
-    print(data.shape)
+    train_data , test_data , train_labels , test_labels =load_MNIST_dataset()
+    print(test_labels.shape)
+
+    new_labels = convert_one_hot_encoding(train_labels)
+    print(new_labels[0],train_labels[0])
+
+    train_data , test_data , train_labels , test_labels =make_MNIST_dataset_small()
+    plt.imshow(train_data[0])
+    plt.show()
